f1304b6a0c296eab2881db6379433e9a5c0b8bcd
tanyq1986 <tanyq1986@126.com>
2019-07-04 18:20:32 +0800

    no

--- lib/pages/tabs/Category.dart
+++ lib/pages/tabs/Category.dart
@@ -7,6 +7,36 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('this is Category'));
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment:MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Expanded(
+                child: TabBar(
+                   tabs: <Widget>[
+                   Tab(text: 'hot'),
+                    Tab(text: 'guess you like',)
+            ],
+           ),
+              )
+            ],
+          ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Container(
+                color: Colors.pinkAccent,
+              ),
+              Container(
+                color: Colors.green,
+              )
+            ],
+          ),
+      ),
+    );
   }
 }
